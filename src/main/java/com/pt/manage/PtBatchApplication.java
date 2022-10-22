2da1bea0d9b5537f59274ff314186ca90e1525ca
eric <shpgod94@naver.com>
2022-10-22 19:40:53 +0900

    first commit

---
+++ src/main/java/com/pt/manage/PtBatchApplication.java
@@ -0,0 +1,13 @@
+package com.pt.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtBatchApplication.class, args);
+	}
+
+}
