99d2415601929b24af5dd765e1c6414890e8234c
eric <shpgod94@naver.com>
2022-11-20 15:01:52 +0900

    feat -최신화

--- src/main/java/com/pt/manage/config/BatchConfig.java
+++ src/main/java/com/pt/manage/config/BatchConfig.java
@@ -1,9 +1,19 @@
 package com.pt.manage.config;
 
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.configuration.support.MapJobRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableBatchProcessing
 @Configuration
 public class BatchConfig {
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry);
+        return postProcessor;
+    }
 }
